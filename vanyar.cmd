df375ff8143043e04799141c260b08423d697ad0
Johan Berntsson <johan.mayumi@gmail.com>
2026-09-23 15:06:10 +0900

    Added cheap_scenery

--- vanyar.cmd
+++ vanyar.cmd
@@ -39,21 +39,72 @@ take box
 ne
 e
 n
+x grass
+x trunks
 w
+x mountains
+x plain
+x forest
+x castle
 n
+x forest
+x grass
 take branch
+eat berries
+eat berries
+eat berries
+eat berries
+eat berries
+eat berries
+eat berries
+eat berries
 s
 sw
+x pass
+x mountain sides
+x crevice
+x rock face
 u
 burn branch
+x cave
 take axe
 extinguish branch
 down
 w
-ne
-ne
-ne
+x hill
+x pass
+x forest
+x crowns
+x desert
+n
+x forest
+s
+w
+x desert
+x sun
+w
+x sand
+x wind
 e
+e
+eat ham
+eat ham
+eat ham
+ne
+x forest
+x desert
+x path
+ne
+x glade
+x opening
+x forest
+ne
+x hall
+x gate
+e
+x cabinets
+x stone table
+x ravine
 w
 sw
 sw
@@ -62,40 +113,116 @@ e
 ne
 e
 e
+x grass
+x forest
+x path
+x shadows
+x plain
+x towers
 e
-e
-n
-chop tree with axe
-chop tree with axe
-e
+x forest
+x path
+x gulls
 s
+x shore
+x pines
+x forest
+x plain
+n
+e
+x forest
+n
+x reeds
+x point
+x bank
+x mountains
+n
+x reeds
+x bank
+x mountain
+n
+x forest
+x mountains
+x cliff
+w
+x castle
+x sea
+x peaks
+x plain
+x river
+x ravine
+e
+sw
+x forest
+s
+x forest
+e
+chop tree with axe
+chop tree with axe
+e
+x tree
+x bank
+s
+x shore
 take rope
 n
 n
+x beach
 n
+x shore
 e
+x cave
 burn branch
 e
+x droppings
+x light
 kill bats
 throw rope at hole
 up
+x passage
+x hole
+x droppings
 e
+x hall
+x stairway
+x passages
 up
+x stairs
+x landing
 up
+x window
+x sunlight
+x landscape
+x peaks
+x forest
+x castles
 take parchment
 down
 down
 s
+x throne
 e
 take herb
 w
 n
 n
+x passage
 w
+x footprints
+x damp ground
 w
+x ravine
+x walls
+x sound
 nw
+x ravine
+x walls
 sw
+x ravine
+x walls
 s
+x ravine
+x rock face
 sw
 eat bread
 eat bread
@@ -105,14 +232,28 @@ n
 ne
 say algeothel
 s
+x passage
 s
+x cellar
+x stair
 up
+x courtyard
+x buildings
+x stairs
+x gates
 up
+x chairs
+x gate
+x stairs
 push sideboard
 w
+x armour
 take broadsword
 e
 up
+x gate
+x tail
+x stairs
 sw
 z
 z
@@ -123,21 +264,75 @@ kill dragon
 kill dragon
 kill dragon
 kill dragon
+kill dragon
+kill dragon
+x balustrade
+x soot
+x forest
+x stairway
+x gate
 e
 kill draupner
 kill draupner
+kill draupner
+kill draupner
 take diamond
+x throne
+x windows
 w
 ne
 down
+e
+x paintings
+w
 down
+w
+x cells
+x gate
+x skeletons
+x rats
+e
+e
+x hearth
+x smoke
+w
+s
+x castle
+x tower
+x banner
+x gate
+x wall
+x courtyard
+s
+x plain
+x castle
+x cliff
+x forest
+w
+x glade
+x hut
+x bones
+x path
+x forest
+w
+x hut
+x fire
+x remains
+e
+e
+s
+x forest
+x undergrowth
+w
+x forest
+x plain
+e
 s
 s
-s
-s
-s
+x plain
+x grass
+x breeze
+x forest
 w
 quit
 yes
-
-
